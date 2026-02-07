cc7467c15b7990f3d65c0e32084cbaee5a259537
thanhtam2842007-collab <thanhtam2842007@gmail.com>
2026-02-07 10:28:37 +0700

    Lab6

--- bai2.c
+++ bai2.c
@@ -9,3 +9,33 @@
 //  Output: Xuất ra màn hình giá trị nhỏ nhất và lớn nhất của mảng
 
 // VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+int main() {
+    int n;
+    
+    printf("Nhập số lượng phần tử của mảng: ");
+    scanf("%d", &n);
+    
+    int mang[n]; // Giả sử mảng có tối đa 100 phần tử
+
+    printf("Nhập các phần tử của mảng:\n"); 
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &mang[i]);
+    }
+
+    int max = mang[0];
+    int min = mang[0];
+    for (int i = 1; i < n; i++) {
+        if (mang[i] > max) {
+            max = mang[i];
+        }
+        if (mang[i] < min) {
+            min = mang[i];
+        }
+    }
+
+    printf("Giá trị lớn nhất trong mảng là: %d\n", max);
+    printf("Giá trị nhỏ nhất trong mảng là: %d\n", min);
+
+    return 0;
+}
